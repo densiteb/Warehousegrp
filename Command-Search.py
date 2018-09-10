6f817c851bafddcbea67f3f7400b0ee086f7d753
tee68 <42766238+tee68@users.noreply.github.com>
2018-09-10 15:03:49 +0700

    Add files via upload

---
+++ Command-Search.py
@@ -0,0 +1,27 @@
+def Search(x):
+    if '''warehouse1''':
+        if '''product_id''' in '''warehouse1'''and '''warehouse1'''['''product_id'''] != "none":
+            print("Found the product a :"+'''warehouse1'''['''product_id'''])
+        elif '''product_id''' in '''warehouse4'''and '''warehouse4'''['''product_id'''] != "none":
+            print("Found the product a :" '''warehouse4'''['''product_id'''])
+        elif ('''product_id''' not in  '''warehouse1''' and '''product_id''' not in '''warehouse4''') or( '''warehouse1'''['''product_id'''] == "none"or '''warehouse4'''['''product_id'''] == "none"):
+            print ("Product not found")
+    elif '''warehouse2''':
+        if '''product_id''' in '''warehouse2'''and '''warehouse2'''['''product_id'''] != "none":
+            print("Found the product a :"+'''warehouse2'''['''product_id'''])
+        elif '''product_id''' in '''warehouse4'''and '''warehouse4'''['''product_id'''] != "none":
+            print("Found the product a :"'''warehouse4'''['''product_id'''])
+        elif ('''product_id''' not in  '''warehouse1''' and '''product_id''' not in '''warehouse4''')or( '''warehouse1'''['''product_id'''] == "none"or '''warehouse4'''['''product_id'''] == "none"):
+            print ("Product not found")
+    elif '''warehouse3''':
+        if '''product_id''' in '''warehouse3''' and '''warehouse3'''['''product_id'''] != "none":
+            print("Found the product a :"+'''warehouse3'''['''product_id'''])
+        elif '''product_id''' in '''warehouse4'''and '''warehouse4'''['''product_id'''] != "none":
+            print("Found the product a :"'''warehouse4'''['''product_id'''])
+        elif ('''product_id''' not in  '''warehouse1''' and '''product_id''' not in '''warehouse4''')or( '''warehouse1'''['''product_id'''] == "none"or '''warehouse4'''['''product_id'''] == "none"):
+            print ("Product not found")
+    elif '''warehouse5''':
+        if '''warehouse5'''['''product_id'''] == "none":
+            print("Found the product a :"+'''warehouse5'''['''product_id'''])
+        elif '''warehouse5'''['''product_id'''] == "none":
+            print ("Product not found")
